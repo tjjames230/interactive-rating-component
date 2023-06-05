12759b129f586e69007ddf8b5a2aa610d198bb1c
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2023-06-05 14:00:24 -0500

    updated buttons

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.querySelector(".circle-hover").addEventListener("click", function () {
-  document.querySelector(".circle-hover").style.backgroundColor = "#f4f4f4";
+let circleHover = document.querySelectorAll(".circle-hover");
+
+function runEvent(event) {
+  circleHover.forEach((element) => {
+    element.style.backgroundColor = "hsl(218, 16%, 23%)";
+  });
+
+  event.target.style.backgroundColor = "hsl(216, 12%, 54%)";
+}
+
+circleHover.forEach((element) => {
+  element.addEventListener("click", runEvent);
 });
