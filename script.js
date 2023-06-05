260e136deb696fdba69778b58a16ae63bd3c1e6d
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2023-06-05 15:46:56 -0500

    updated submit btn

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let circleHover = document.querySelectorAll(".circle-hover");
+let result = document.querySelector("#result");
+let btn = document.querySelector("#btn");
 
 function runEvent(event) {
   circleHover.forEach((element) => {
@@ -6,8 +8,20 @@ function runEvent(event) {
   });
 
   event.target.style.backgroundColor = "hsl(216, 12%, 54%)";
+  result.innerHTML = event.target.innerHTML;
 }
 
 circleHover.forEach((element) => {
   element.addEventListener("click", runEvent);
 });
+
+function submit() {
+  if (result.innerHTML == 0) {
+    document.querySelector("#warning").style.display = "block";
+  } else {
+    document.querySelector("#first-card-ctn").style.display = "none";
+    document.querySelector("#second-card-ctn").style.display = "flex";
+  }
+}
+
+btn.addEventListener("click", submit);
